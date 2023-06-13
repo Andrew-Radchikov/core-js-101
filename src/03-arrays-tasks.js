5020c8e352a17da1e9d12e988ca3106c5da2f5b4
Andrew-Radchikov <superprobasite@yandex.ru>
2023-06-13 22:35:14 +0300

    feat: new part of tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -279,8 +279,8 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((rez, value, index) => rez.concat(new Array(index + 1).fill(value)), []);
 }
 
 
@@ -297,8 +297,13 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  arr.sort((a, b) => b - a);
+  if (arr.length >= 3) {
+    return arr.splice(0, 3);
+  }
+
+  return arr;
 }
 
 
@@ -315,8 +320,17 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return arr.reduce((sum, value) => {
+    let x = sum;
+    if (typeof value === 'number') {
+      if (value > 0) {
+        x += 1;
+      }
+    }
+
+    return x;
+  }, 0);
 }
 
 /**
@@ -332,8 +346,17 @@ function getPositivesCount(/* arr */) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+
+function sortMyText(a, b) {
+  const numberArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const x = numberArray.indexOf(a);
+  const y = numberArray.indexOf(b);
+  return x - y;
+}
+
+function sortDigitNamesByNumericOrder(arr) {
+  arr.sort((a, b) => sortMyText(a, b));
+  return arr;
 }
 
 /**
@@ -348,8 +371,8 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
+function getItemsSum(arr) {
+  return arr.reduce((sum, value) => sum + value, 0);
 }
 
 /**
@@ -364,8 +387,15 @@ function getItemsSum(/* arr */) {
  *  [ -1, 'false', null, 0 ] => 2
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((sum, value) => {
+    let x = sum;
+    if (!value) {
+      x += 1;
+    }
+
+    return x;
+  }, 0);
 }
 
 /**
@@ -382,8 +412,15 @@ function getFalsyValuesCount(/* arr */) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((sum, value) => {
+    let x = sum;
+    if (value === item) {
+      x += 1;
+    }
+
+    return x;
+  }, 0);
 }
 
 /**
@@ -397,8 +434,8 @@ function findAllOccurrences(/* arr, item */) {
  *    [1, 2, 3, 4, 5]                   => '1,2,3,4,5'
  *    ['rock', 'paper', 'scissors']     => 'rock,paper,scissors'
  */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  return arr.join(',');
 }
 
 
